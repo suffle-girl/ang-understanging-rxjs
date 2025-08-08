5d3ea60d247f666f54c8058d55e3b32ae91bff33
Šárka Flídrová <sarka.flidrova@email.cz>
2025-08-08 10:21:42 +0200

    using operators - pipe method with map to transform a subscribed value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, DestroyRef, inject, OnInit } from "@angular/core";
-import { interval } from "rxjs";
+import { interval, map } from "rxjs";
 
 @Component({
   selector: "app-root",
@@ -10,9 +10,11 @@ export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
-    const subscription = interval(1000).subscribe({
-      next: (value) => console.log("next value: ", value),
-    });
+    const subscription = interval(1000)
+      .pipe(map((val) => val * 2))
+      .subscribe({
+        next: (value) => console.log("next value: ", value),
+      });
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
